009633a7a125ab7e6875955be8616abdce4cdef0
Diego <diego.santos@besistemas.com.br>
2024-04-03 17:26:34 -0300

    feat: removing unused components/pages

--- frontend/src/components/Toast.jsx
+++
@@ -1,7 +0,0 @@
-import { toast, ToastContainer } from "react-toastify";
-
-function Toast({ message }) {
-  return toast.success(message, { position: "bottom-center", theme: "dark" });
-};
-
-export default Toast;
