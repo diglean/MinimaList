e6152e26415fbc66fefa631a7e497da2c5eaea79
Diego <diego.santos@besistemas.com.br>
2024-06-21 16:35:03 -0300

    WIP: Profile - Some options added

--- frontend/src/pages/profile/Profile.jsx
+++ frontend/src/pages/profile/Profile.jsx
@@ -1,7 +1,7 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, List, ListItemButton, ListItemText, Typography } from "@mui/material";
 import AppBar from "../../components/LogoBar";
 import styles from "./styles/Profile.module.css";
-import CustomPaper from "../../components/Paper";
+import HistoryIcon from '@mui/icons-material/History';
 
 export default function Profile() {
   return (
@@ -9,7 +9,6 @@ export default function Profile() {
       <AppBar goBack="/"/>
       <div className={styles.container}>
         <div className={styles.profile_container}>
-
           <Grid container spacing="10">
             <Grid item sx={4}>
               <Box
@@ -27,7 +26,14 @@ export default function Profile() {
             <Grid item sx={8}>
               <Grid container>
                 <Grid item sx={12} className={styles.name_container}>
-                  <Typography component="h5" sx={{ color: "#FFF", fontSize: "19px" }}>
+                  <Typography component="h5" sx={{ color: "#FFF", fontSize: "20px" }}>
+                    Diego Leandro
+                  </Typography>
+                </Grid>
+              </Grid>
+              <Grid container>
+                <Grid item sx={12} className={styles.name_container}>
+                  <Typography component="h5" sx={{ color: "#888", fontSize: "17px" }}>
                     Diego Leandro
                   </Typography>
                 </Grid>
@@ -35,6 +41,28 @@ export default function Profile() {
             </Grid>
           </Grid>
         </div>
+      </div>
+      <div className={styles.options_container}>
+        <List>
+          {/* Lists history */}
+          <ListItemButton
+            disableRipple
+            key="history"
+          >
+            <HistoryIcon />
+            <ListItemText primary="Histórico" />
+          </ListItemButton>
+          <Divider variant="middle" component="li" />
+          {/* Relatórios */}
+          <ListItemButton
+            disableRipple
+            key="history"
+          >
+            <HistoryIcon />
+            <ListItemText primary="Relatórios" />
+          </ListItemButton>
+          <Divider variant="middle" component="li" />
+        </List>
       </div>
     </div>
   );
